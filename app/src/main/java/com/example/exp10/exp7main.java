69502309f54bb6a6628924e1777544809febe0ca
Harika-Devarapalli <devarapalliharika01@gmail.com>
2022-05-25 12:24:58 +0530

    exp10 halff

--- app/src/main/java/com/example/exp10/exp7main.java
+++ app/src/main/java/com/example/exp10/exp7main.java
@@ -35,7 +35,7 @@ public class exp7main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_exp7main);
 
         manager = NotificationManagerCompat.from(getApplicationContext());
         title = findViewById(R.id.title);
